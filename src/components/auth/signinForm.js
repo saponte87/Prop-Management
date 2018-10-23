ba753dd7249664eb4aea4f83cbcc22a5f72376e1
Stephen Aponte <saponte1987@gmail.com>
2018-10-23 09:49:24 -0600

    styled formed input

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'react-form';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
-import { FormTitle } from '../formTitle';
-import { FormInput } from '../formFields';
+import { FormTitle } from "../formTitle";
+import { FormInput } from "../formFields";
 
 class SigninForm extends Component {
-    render() {
-        return (
-            <form className='sign-in-form'>
-                <FormTitle className='sign-in_form__title' text='Login'/>
-                <Field conponent='sign-in-form__email' name='email' type='email'/>            
-            </form>
-        )
-    }
-
-    SigninForm = reduxForm({
-        form: 'signin'
-    })(SigninForm);
+  render() {
+    return (
+      <form className="sign-in-form">
+        <FormTitle className="sign-in-form__title" text="Login" />
+        <Field
+          className="sign-in-form__email"
+          placeholder="Enter Email"
+          name="email"
+          type="email"
+          title="Email"
+          component={FormInput}
+        />
+        <Field
+          className="sign-in-form__password"
+          placeholder="Enter Password"
+          name="password"
+          type="password"
+          title="Password"
+          component={FormInput}
+        />
+      </form>
+    );
+  }
 }
 
-export default SigninForm
+SigninForm = reduxForm({
+  form: "signin"
+})(SigninForm);
