2101cc6d4b4bee7269471ec2d247805f8add7549
Stephen Aponte <saponte1987@gmail.com>
2018-10-25 11:48:39 -0600

    created and placed add newletter button on newsletter grid

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -1,14 +1,5 @@
 import React, { Component } from 'react';
-
-function Button({callback, text, icon, className}) {
-    if(icon) {
-        return (
-           <a onClick={callback} className={`${className} button`}>
-                <i className={icon}></i>
-           </a> 
-        )
-    }
-}
+import Button from '../button';
 
 class NewsletterLatest extends Component {
 
@@ -22,7 +13,7 @@ class NewsletterLatest extends Component {
             <div className='newsletter-latest'>
                 <h1 className='newsletter-latest__title'>{title}</h1>
                 <img className='newsletter-latest__image' src={imageUrl}/>
-                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon='fas fa-pencil-alt'/> 
+                <Button className='newsletter-latest__button' callback={() => this.handleEdit()} icon='fas fa-pencil-alt'/>
                 <div className='newsletter-latest__body'>
                     <p>{body}</p>
                 </div>
